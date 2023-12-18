ea3a094118ae8be1b4eb8ce4379ef92897c54627
JustCryen <41633661+JustCryen@users.noreply.github.com>
2023-12-18 14:42:15 +0100

    File check added to the save file

--- iverilog-run.sh
+++ iverilog-run.sh
@@ -1,36 +1,37 @@
 #! /bin/bash
 
-dir=$(pwd)
-cd "$dir"
+save_file=$(echo -e "$(pwd)/output/save")
 
-if ! [ -d "$dir/output" ]; then
+if ! [ -d "$(pwd)/output" ]; then
 	echo -e "Created an output dir.\t" 
 	mkdir output
 fi
 
-if [ -f "$dir/output/save" ]; then
+if [ -f "$save_file" ]; then
 	read -p 'Do you want to exec the save file? [Y/n]: ' save
 	case $save in
 		[nN] | [nN][oO])
 			;;
 		[yY] | [yY][eE][sS] | *)
-				exec "$dir/output/save" "$1" >&2; exit 1
+				exec "$save_file" "$1" >&2; exit 1
 			;;
 	esac
 fi
 
-echo  "iverilog -o output/<file>.vvp <files>.v"
+echo "iverilog -o output/<file>.vvp <files>.v"
 read -ep 'Output file name: ' vvp_name
 read -ep 'verilog files: ' v_files
 echo ""
+
+lxt_file=$(echo -e "$dir/output/$vvp_name.lxt")
 
 echo -e "> iverilog -o output/$vvp_name.vvp $v_files\n"
 command="iverilog -Wall -o output/$vvp_name.vvp $v_files"
 iverilog -Wall -o output/$vvp_name.vvp $v_files || exit 1
 
 
-if [ -f "$dir/output/$vvp_name.lxt" ]; then
-  rm "$dir/output/$vvp_name.lxt"
+if [ -f "$lxt_file" ]; then
+  rm "$lxt_file"
 fi
 
 
@@ -49,7 +50,7 @@ fi
 read -ep 'Do you want to save the config file? [y/N]: ' save
 case $save in
 	[yY] | [yY][eE][sS])
-		if [ -f "$dir/output/save" ]; then
+		if [ -f "$save_file" ]; then
 			read -p 'Overwrite the save file? [y/N]: ' overwrite
 			case $overwrite in
 				[yY] | [yY][eE][sS])
@@ -59,13 +60,15 @@ case $save in
 					;;
 			esac
 		fi
-		echo -e "#! /bin/bash\n" > "$dir/output/save"
-		echo -e "rm '$dir/output/$vvp_name.lxt'" >> "$dir/output/save"
-		echo -e "$command" >> "$dir/output/save"
-		echo 'if ! [[ $1 == "-q" ]]; then' >> "$dir/output/save"
-		echo -e "\t$render" >> "$dir/output/save"
-		echo -e "fi" >> "$dir/output/save"
-		chmod +x "$dir/output/save" & echo "Saved"
+		echo -e "#! /bin/bash\n"				>  "$save_file"
+		echo -e "if [ -f $lxt_file ]; then"		>> "$save_file"
+		echo -e "\trm $lxt_file"				>> "$save_file"
+		echo "fi"								>> "$save_file"
+		echo -e "$command"						>> "$save_file"
+		echo 'if ! [[ $1 == "-q" ]]; then'		>> "$save_file"
+		echo -e "\t$render"						>> "$save_file"
+		echo "fi"								>> "$save_file"
+		chmod +x "$save_file" & echo "Saved"
 		;;
 	[nN] | [nN][oO] | *)
 		;;
